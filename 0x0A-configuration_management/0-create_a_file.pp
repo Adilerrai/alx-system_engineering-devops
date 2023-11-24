480659ebab6d8d215a9a04b1a4b97775b385b57f
Adilfettach <adilerrai24@gmail.com>
2023-11-24 17:07:55 +0100

    fixing the content

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 #this resource of puppet to create a file with content
 File {'/etc/school':
-group => 'www-data',
-owner => 'www-data',
-mode => '0744',
-content => 'I love Puppet',
+  ensure  => 'file',
+  group => 'www-data',
+  owner => 'www-data',
+  mode => '0744',
+  content => 'I love puppet',
 }
