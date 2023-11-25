80843f97088f1751e9a6a800a36eb16b81b04e46
Adilfettach <adilerrai24@gmail.com>
2023-11-25 14:59:59 +0100

    fixxing the puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,12 @@
 #this resource File_line is to check and add a line to change configuration with some specs
 file_line { 'Turn off passwd auth':
-ensure            => 'present'
-path              => '~/etc/ssh/ssh_config',
-match             => '^export # PasswordAuthentication yes',
-match_for_absence => false,
-replace           => ' PasswordAuthentication no '
+path  => '~/etc/ssh/ssh_config',
+match => '# PasswordAuthentication yes',
+line  => ' PasswordAuthentication no '
 }
 file_line { 'Declare identity file':
-ensure            => 'present'
-path              => '~/etc/ssh/ssh_config',
-match             => '^export # IdentityFile ',
-match_for_absence => false,
-replace           => ' IdentityFile ~/.ssh/school '
+ensure  => 'present'
+path    => '~/etc/ssh/ssh_config',
+match   => ' # IdentityFile ',
+replace => ' IdentityFile ~/.ssh/school '
 }
