3f745e38971a4f4f4c0517453f828211a79cccd4
Adilfettach <adilerrai24@gmail.com>
2023-11-25 12:26:46 +0100

    fixing the flag of exec resource

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,4 @@
 # This resource is to exec a command 
-exec {'killmenow':
-command => 'pkill -f killmenow',
-onlyif  => 'pgrep -f killmenow',
+exec { 'killmenow':
+  command => 'pkill -f killmenow',
 }
